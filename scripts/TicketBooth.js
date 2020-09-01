1cfe73c8e9a83cb9302fbe85ff375c5737a99cac
Ricky Blake <rab0196@yahoo.com>
2020-09-01 13:28:55 -0500

    completed setup

--- scripts/TicketBooth.js
+++ scripts/TicketBooth.js
@@ -1,12 +1,17 @@
 const contentTarget = document.querySelector(".entry")
 const eventHub = document.querySelector("#state-fair")
 
-eventHub.addEventListener()
+eventHub.addEventListener("click", event => {
+    if(event.target.id === "rideTicket") {
+        const rideEvent = new CustomEvent("rideTicketPurchased")
+        eventHub.dispatchEvent(rideEvent)
+    }
+})
 
 export const TicketBooth = () => {
     contentTarget.innerHTML = `
         <div class="ticketBooth">
-            Add button here
+            <button id="rideTicket">Ride Ticket</button>
         </div>
     `
 }
