41884745cda94d4052968379627789708021e74a
Ricky Blake <rab0196@yahoo.com>
2020-09-02 12:05:27 -0500

    added food tickets

--- scripts/TicketBooth.js
+++ scripts/TicketBooth.js
@@ -8,10 +8,20 @@ eventHub.addEventListener("click", event => {
     }
 })
 
+eventHub.addEventListener("click", event => {
+    if(event.target.id === "foodTicket") {
+        const foodEvent = new CustomEvent("foodTicketPurchased")
+        eventHub.dispatchEvent(foodEvent)
+    }
+})
+
 export const TicketBooth = () => {
     contentTarget.innerHTML = `
         <div class="ticketBooth">
             <button id="rideTicket">Ride Ticket</button>
+        </div>
+        <div class="ticketBooth">
+            <button id="foodTicket">Food Ticket</button>
         </div>
     `
 }
